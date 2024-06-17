947e91005bc0a25dc2a06413c399db8b56a49b1d
johnson-oragui <johnson-oragui@gmail.com>
2024-06-17 09:10:40 +0100

    changed inheritance to base_model_archive and added user_id attribute

--- models/archived_user.py
+++ models/archived_user.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, String, DateTime
-# from sqlalchemy.orm import relationship
-from .base_model import Base, BaseModel
+from sqlalchemy import Column, String, DateTime, Integer
+from .base_model import Base
+from .base_model_archive import BaseModelArchive
 
-class ArchivedUser(BaseModel, Base):
+class ArchivedUser(BaseModelArchive, Base):
     __tablename__ = 'archived_users'
 
+    user_id = Column(String(60), nullable=False)
     first_name = Column(String(60), nullable=False)
     last_name = Column(String(60), nullable=False)
     username = Column(String(60), nullable=False, unique=True)
