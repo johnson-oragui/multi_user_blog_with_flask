a59750c36ad32f4998291f46e48f16c9de7d2376
johnson-oragui <johnson-oragui@gmail.com>
2024-06-17 09:12:51 +0100

    inherited from base_model_archive, added blog_id and category attributes

--- models/archived_blog.py
+++ models/archived_blog.py
@@ -1,13 +1,16 @@
-from sqlalchemy import Column, String, DateTime, Boolean
-from .base_model import Base, BaseModel
+from sqlalchemy import Column, String, DateTime, Boolean, Integer
+from .base_model import Base
+from .base_model_archive import BaseModelArchive
 
 
-class ArchivedBlog(BaseModel, Base):
+class ArchivedBlog(BaseModelArchive, Base):
     __tablename__ = 'archived_blogs'
 
+    blog_id = Column(Integer)
     user_id = Column(String(60), nullable=False)
     title = Column(String(60), nullable=False)
     content = Column(String(1000), nullable=False)
+    category = Column(String(60))
     initial_created_at = Column(DateTime)
     initial_updated_at = Column(DateTime)
     is_from_account_deletion = Column(Boolean, default=True)
