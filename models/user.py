33aed7d0111bd111a04cfb9aa660da30b70c6ffc
johnson-oragui <johnson-oragui@gmail.com>
2024-06-18 17:50:41 +0100

    refactored for annotation, added more fields, added validations for email and is_admin

--- models/user.py
+++ models/user.py
@@ -1,12 +1,18 @@
 from flask import g
 import logging
 from uuid import uuid4
-from sqlalchemy import Column, String, event
-from sqlalchemy.orm import relationship
+from email_validator import validate_email, EmailNotValidError, EmailUndeliverableError
+from typing import List
+from sqlalchemy import String, Boolean, event
+from sqlalchemy.orm import relationship, Mapped, mapped_column, validates
 from .base_model import Base, BaseModel
+from .blog import Blog
+from .comment import Comment
+from .custom_type import ThemeAndLanguage
 from .archived_blog import ArchivedBlog
 from .archived_user import ArchivedUser
 from .archived_comment import ArchivedComment
+
 
 logger = logging.getLogger(__name__)
 
@@ -14,19 +20,56 @@ logger = logging.getLogger(__name__)
 class User(BaseModel, Base):
     __tablename__ = 'users'
 
-    id = Column(String(60), primary_key=True, default=str(uuid4()))
-    first_name = Column(String(60), nullable=False)
-    last_name = Column(String(60), nullable=False)
-    username = Column(String(60), nullable=False, unique=True)
-    email = Column(String(60), nullable=False, unique=True)
-    password = Column(String(60), nullable=False)
-    blogs = relationship('Blog',
-                            back_populates='user',
-                            cascade='all, delete-orphan')
-    comments = relationship('Comment',
-                            back_populates='user',
-                            cascade='all, delete-orphan')
+    id: Mapped[str] = mapped_column(String(60), primary_key=True, default=lambda: str(uuid4()))
+    first_name: Mapped[str] = mapped_column(String(60), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(60), nullable=False)
+    username: Mapped[str] = mapped_column(String(60), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(60), nullable=False, unique=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    password: Mapped[str] = mapped_column(String(60), nullable=False)
+    is_admin: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    reg_token: Mapped[str] = mapped_column(String(35))
+    preferences: Mapped[dict] = mapped_column(ThemeAndLanguage, default={'theme': 'light', 'language': 'en'})
 
+    blogs: Mapped[List[Blog]] = relationship(backref='user', cascade='all, delete-orphan')
+    comments: Mapped[List['Comment']] = relationship(backref='user', cascade='all, delete-orphan')
+    # blogs: Mapped[List['Blog']] = relationship('Blog', backref='user', cascade='all, delete-orphan')
+    # comments: Mapped[List['Comment']] = relationship('Comment', backref='user', cascade='all, delete-orphan')
+
+    def __str__(self):
+        user_data = {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "username": self.username,
+            "email": self.email,
+            "blogs": self.blogs,
+            "comments": self.comments,
+        }
+        return f'{{ {", ".join(f"{k}: {v}" for k, v in user_data.items())} }}'
+    
+    @validates('is_admin')
+    def validate_is_admin(self, key, is_admin):
+        '''prevents unauthorized modifications'''
+        if key:
+            raise ValueError(f'cannot set {is_admin}')
+        if is_admin:
+            raise ValueError(f'cannot set {is_admin}')
+    
+    @validates('email')
+    def validate_email(self, key, address):
+        try:
+            is_valid = validate_email(address, check_deliverability=True)
+            normalized_email = is_valid.normalized
+            print('email validated: ', is_valid)
+            return normalized_email
+        except EmailUndeliverableError as exc:
+            logger.error(f'an exception occured during email validation {exc}')
+        except EmailNotValidError as exc:
+            logger.error(f'email is invalid: {exc}')
+        except ValueError as exc:
+            logger.error(f'an exception occured during email validation {exc}')
+        
 
 def handle_user_deletion(mapper, connection, target):
     '''Archive user before deletion'''
@@ -40,6 +83,9 @@ def handle_user_deletion(mapper, connection, target):
     except AttributeError:
         pass
     print('hook running now for user deletion: ')
+
+    if target.is_admin:
+        return
 
     # check for blogs associated with the user
     if target.blogs:
