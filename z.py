cc3e6b4ae13c50110857fbef6ae2e08dd33b37c4
johnson-oragui <johnson-oragui@gmail.com>
2024-06-18 17:58:02 +0100

    this is not part of the project ish

--- z.py
+++ z.py
@@ -1,74 +1,69 @@
-import time
-from models import DBStorage
-from models.user import User
-from models.blog import Blog
-from models.comment import Comment
-from models.archived_user import ArchivedUser
-from models.archived_blog import ArchivedBlog
-from models.archived_comment import ArchivedComment
-from app import flask_app, g
+# import time
+# from models.db_engine.db_storage import DBStorage
+# from app import flask_app, g
 
 
-f_name = 'Prince'
-l_name = 'Dennis'
-username = 'Prince'
-email = 'Prince@email.com'
-password = 'Prince'
+# f_name = 'Prince'
+# l_name = 'Dennis'
+# username = 'Prince'
+# email = 'Prince@email.com'
+# password = 'Prince'
 
-user_dict = {
-    'first_name': f_name,
-    'last_name': l_name,
-    'username': username,
-    'email': email,
-    'password': password
-}
+# user_dict = {
+#     'first_name': f_name,
+#     'last_name': l_name,
+#     'username': username,
+#     'email': email,
+#     'password': password,
+#     'preferences': {'theme': 'dark', 'language': 'en'}
+# }
 
-title = 'title 1'
-content = 'first blog'
-category = 'None'
+# title = 'title 1'
+# content = 'first blog'
+# category = 'None'
 
-blog_dict = {
-    'title': title,
-    'content': content,
-    'category': category
-}
+# blog_dict = {
+#     'title': title,
+#     'content': content,
+#     'category': category
+# }
 
-comment = 'This is a comment'
+# comment = 'This is a comment'
 
-app = flask_app()
+# app = flask_app()
 
-user_updates = {
-            'first_name': 'new fname',
-            'last_name': 'new lastname',
-}
-blog_updates = {
-    'title': 'Updated Title',
-    'content': 'Updated Content',
-}
-comment_updates = 'Updated Comment'
+# user_updates = {
+#             'first_name': 'new fname',
+#             'last_name': 'new lastname',
+# }
+# blog_updates = {
+#     'title': 'Updated Title',
+#     'content': 'Updated Content',
+# }
+# comment_updates = 'Updated Comment'
 
-with app.app_context():
-    with DBStorage() as db:
-        g.user_deletion = True
+# with app.app_context():
+#     with DBStorage() as db:
+#         g.user_deletion = True
     
-        db.drop_all_tables()
-        time.sleep(2)
-        db.create_all_tables()
-        time.sleep(2)
+#         db.drop_all_tables()
+#         time.sleep(2)
+#         db.create_all_tables()
+#         time.sleep(2)
 
-        print()
-        print()
+#         print()
+#         print()
 
 
-        new_user = db.handle_user(user_dict=user_dict)
+#         new_user = db.handle_user(user_dict=user_dict)
 
-        print()
+#         print()
 
-        new_blog = db.handle_blog(user_id=new_user.get('id') , blog_dict=blog_dict)
+#         new_blog = db.handle_blog(user_id=new_user.get('id') , blog_dict=blog_dict)
 
-        print()
+#         print()
 
-        new_comment = db.handle_comment(user_id=new_user.get('id'), blog_id=new_blog.get('id'), comment=comment)
+#         new_comment = db.handle_comment(user_id=new_user.get('id'), blog_id=new_blog.get('id'), comment=comment)
 
         # print()
 
@@ -76,7 +71,7 @@ with app.app_context():
         # print()
         # print()
 
-        # # Update user
+        # Update user
         # updated_user = db.handle_user(user_id=new_user['id'], user_dict=user_updates, edit=True)
         # print(f'Updated User: {updated_user}')
 
@@ -88,10 +83,10 @@ with app.app_context():
         # updated_comment = db.handle_comment(user_id=new_user['id'], blog_id=new_blog['id'], comment_id=new_comment['id'], comment=comment_updates, edit=True)
         # print(f'Updated Comment: {updated_comment}')
 
-        time.sleep(2)
+        # time.sleep(2)
 
-        print()
-        print()
+        # print()
+        # print()
 
         # comment_to_del = db.handle_comment(user_id=new_user['id'], blog_id=new_blog['id'], comment_id=new_comment['id'], delete=True)
         # print('comment_to_del: ', comment_to_del)
@@ -99,7 +94,5 @@ with app.app_context():
         # blog_to_del = db.handle_blog(user_id=new_user['id'], blog_id=new_blog['id'], delete=True)
         # print('blog_to_del: ', blog_to_del)
 
-        user_to_del = db.handle_user(user_id=new_user['id'], delete=True)
-        print('user_to_del: ', user_to_del)
-
-
+        # user_to_del = db.handle_user(user_id=new_user['id'], delete=True)
+        # print('user_to_del: ', user_to_del)
