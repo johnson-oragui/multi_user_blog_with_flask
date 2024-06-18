5ad6a8bee05d473ffb55188681053b953ad0be26
johnson-oragui <johnson-oragui@gmail.com>
2024-06-18 17:44:19 +0100

    refactored for annotations

--- models/archived_user.py
+++ models/archived_user.py
@@ -1,15 +1,18 @@
-from sqlalchemy import Column, String, DateTime, Integer
+from datetime import datetime
+from sqlalchemy import String, DateTime, Boolean
+from sqlalchemy.orm import mapped_column, Mapped
 from .base_model import Base
 from .base_model_archive import BaseModelArchive
 
 class ArchivedUser(BaseModelArchive, Base):
     __tablename__ = 'archived_users'
 
-    user_id = Column(String(60), nullable=False)
-    first_name = Column(String(60), nullable=False)
-    last_name = Column(String(60), nullable=False)
-    username = Column(String(60), nullable=False, unique=True)
-    email = Column(String(60), nullable=False, unique=True)
-    password = Column(String(60), nullable=False)
-    initial_created_at = Column(DateTime, nullable=False)
-    initial_updated_at = Column(DateTime, nullable=False)
+    user_id: Mapped[int] = mapped_column(String(60), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(60), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(60), nullable=False)
+    username: Mapped[str] = mapped_column(String(60), nullable=False)
+    email: Mapped[str] = mapped_column(String(60), nullable=False)
+    password: Mapped[str] = mapped_column(String(60), nullable=False)
+    is_admin: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    initial_created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    initial_updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
