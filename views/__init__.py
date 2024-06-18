f9e13f63f92cec2364d26047d238397a95a9d69a
johnson-oragui <johnson-oragui@gmail.com>
2024-06-18 17:55:15 +0100

    created another blueprint for admin

--- views/__init__.py
+++ views/__init__.py
@@ -3,8 +3,10 @@ from flask import Blueprint
 authenticate = Blueprint('authenticate', __name__)
 dashboard = Blueprint('dashboard', __name__)
 views = Blueprint('views', __name__)
+admin = Blueprint('admin', __name__)
 
 
 from . import comments, auth
 from . import users, blogs
 from . import home, about
+from . import admin
