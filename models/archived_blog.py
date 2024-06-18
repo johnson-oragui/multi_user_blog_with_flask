2e25077273627c36c66ad36e718ba61df8b54217
johnson-oragui <johnson-oragui@gmail.com>
2024-06-18 17:44:03 +0100

    refactored for annotations

--- models/archived_blog.py
+++ models/archived_blog.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, String, DateTime, Boolean, Integer
+from datetime import datetime
+from sqlalchemy import String, DateTime, Boolean, Integer
+from sqlalchemy.orm import mapped_column, Mapped
 from .base_model import Base
 from .base_model_archive import BaseModelArchive
 
@@ -6,17 +8,11 @@ from .base_model_archive import BaseModelArchive
 class ArchivedBlog(BaseModelArchive, Base):
     __tablename__ = 'archived_blogs'
 
-    blog_id = Column(Integer)
-    user_id = Column(String(60), nullable=False)
-    title = Column(String(60), nullable=False)
-    content = Column(String(1000), nullable=False)
-    category = Column(String(60))
-    initial_created_at = Column(DateTime)
-    initial_updated_at = Column(DateTime)
-    is_from_account_deletion = Column(Boolean, default=True)
-
-    # def __init__(self, **kwargs) -> None:
-    #     super(ArchivedBlog, self).__init__(**kwargs)
-    #     self.archived_user_id = kwargs.get('user_id')
-    
-    # comments = relationship('Comment', backref='blogs', cascade='all')
+    blog_id: Mapped[int] = mapped_column(Integer)
+    user_id: Mapped[str] = mapped_column(String(60), nullable=False)
+    title: Mapped[str] = mapped_column(String(60), nullable=False)
+    content: Mapped[str] = mapped_column(String(1000), nullable=False)
+    category: Mapped[str] = mapped_column(String(60))
+    initial_created_at: Mapped[datetime] = mapped_column(DateTime)
+    initial_updated_at: Mapped[datetime] = mapped_column(DateTime)
+    is_from_account_deletion: Mapped[bool] = mapped_column(Boolean, default=True)
