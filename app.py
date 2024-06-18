5dc34f4bd1812d347586011bb6d8df28726c1956
johnson-oragui <johnson-oragui@gmail.com>
2024-06-18 17:40:56 +0100

    imported and used loadenv for env variables

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import dotenv
 from flask import Flask, g
 from flask_wtf import CSRFProtect
 from configs.config import Config
-from views import views, dashboard, authenticate
+from views import views, dashboard, authenticate, admin
+
+dotenv.load_dotenv()
 
 
 def flask_app():
@@ -17,5 +20,6 @@ def flask_app():
     app.register_blueprint(dashboard, url_prefix='/dashboard')
     app.register_blueprint(authenticate)
     app.register_blueprint(views)
+    app.register_blueprint(admin)
 
     return app
